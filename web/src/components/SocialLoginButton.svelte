466da16e73f5ddd50fa50588a0d618430955644c
오윤석 <dhdbstjr98@khu.ac.kr>
2021-05-21 23:19:56 +0900

    로그인 퍼블리싱

---
+++ web/src/components/SocialLoginButton.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let sns;
+  export let name;
+
+  const handleClick = () => {
+    console.log(sns);
+  };
+</script>
+
+<style>
+  .social-login-button {
+    height: 30px;
+    box-sizing: content-box;
+    border-width: 1px;
+    border-style: solid;
+    vertical-align: middle;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+
+  .social-login-button img {
+    width: 30px;
+    height: 30px;
+    border-right-width: 1px;
+    border-right-style: solid;
+    vertical-align: middle;
+  }
+
+  .social-login-button.naver {
+    background-color: #1fc800;
+    border-color: #1ea505;
+    color: #fff;
+  }
+
+  .social-login-button.kakao {
+    background-color: #ffeb00;
+    border-color: #e2c10a;
+    color: #000;
+  }
+
+  .social-login-button.naver img {
+    border-right-color: #1ea505;
+  }
+
+  .social-login-button.kakao img {
+    border-right-color: #1ea505;
+  }
+
+  .social-login-button span {
+    display: inline-block;
+    padding-left: 5px;
+    font-size: 0.8rem;
+    font-weight: 700;
+  }
+</style>
+
+<div class="social-login-button {sns}" on:click={handleClick}>
+  <img src={`./img/sns_${sns}.png`} alt={sns} width="30" height="30" />
+  <span>{name} 로그인</span>
+</div>
--- web/src/routes/Login.svelte
+++ web/src/routes/Login.svelte
@@ -1,7 +1,43 @@
 <script>
   import Banner from "~/components/Banner.svelte";
   import Body from "~/components/Body.svelte";
+  import SocialLoginButton from "~/components/SocialLoginButton.svelte";
+
+  const snsList = {
+    naver: "네이버",
+    kakao: "카카오"
+  };
 </script>
 
+<style>
+  .login-wrapper {
+    padding: 50px 0;
+  }
+
+  h2 {
+    font-size: 1.6rem;
+    text-align: center;
+    margin-bottom: 1em;
+  }
+
+  .login-form {
+    background-color: #e0f2f1;
+    padding: 20px 10px 10px;
+  }
+</style>
+
 <Banner />
-<Body>login</Body>
+<Body>
+  <div class="row login-wrapper">
+    <div class="col offset-l4 l4 offset-m3 m6 s12">
+      <h2>로그인</h2>
+      <div class="row login-form">
+        <div class="col s12">
+          {#each Object.keys(snsList) as sns}
+            <SocialLoginButton {sns} name={snsList[sns]} />
+          {/each}
+        </div>
+      </div>
+    </div>
+  </div>
+</Body>
